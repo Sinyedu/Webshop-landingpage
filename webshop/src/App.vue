b7db5bd17a7a195e4888cb6abba1d8032174b050
Simon Nyblom <simnyb01@easv365.dk>
2024-06-20 21:39:57 +0200

    Component

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -2,10 +2,12 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
 import HeaderNav from './components/HeaderNav.vue'
+import SpinningContent from './components/SpinningContent.vue'
 </script>
 
 <template>
   <HeaderNav />
+  <SpinningContent />
   <RouterView />
 </template>
 
